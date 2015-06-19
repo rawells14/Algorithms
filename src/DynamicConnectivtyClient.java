a773d9d78c07476e3a33348b95a11099780a291e
Ryan Wells <rawells14@gmail.com>
2015-06-19 14:21:55 -0500

    Finished Quick Find

--- src/DynamicConnectivtyClient.java
+++ src/DynamicConnectivtyClient.java
@@ -4,5 +4,13 @@ import java.util.Scanner;
 public class DynamicConnectivtyClient {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        QuickFind qf = new QuickFind(10);
+        qf.union(0, 5);
+        qf.union(0, 4);
+        qf.union(9, 2);
+        qf.union(4, 6);
+        qf.union(6, 1);
+        qf.union(7, 8);
+        qf.printIDArr();
     }
 }
--- src/QuickFind.java
+++ src/QuickFind.java
@@ -4,17 +4,29 @@ class QuickFind {
     public QuickFind(int n) {
         id = new int[n];
         for (int i = 0; i < n; i++) {
-            id[i] = n;
+            id[i] = i;
         }
         //initialize the id array with indices matching their value
     }
 
     public void union(int p, int q) {
-
+        int pID = id[p];
+        int qID = id[q];
+        for (int i = 0; i < id.length; i++) {
+            if (pID == id[i]) {
+                id[i] = qID;
+            }
+        }
     }
 
 
     public boolean isConnected(int p, int q) {
-        return true;
+        return id[p] == id[q];
+    }
+
+    public void printIDArr() {
+        for (int i = 0; i < id.length; i++) {
+            System.out.print(id[i] + "  ");
+        }
     }
 }
